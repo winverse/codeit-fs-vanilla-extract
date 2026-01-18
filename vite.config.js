bd2789772e1e5fab1088a774aa931df0f65c26a6
winverse <public.winverse@gmail.com>
2026-01-18 15:34:54 +0900

    feat: 01-start

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: ['babel-plugin-react-compiler'],
       },
     }),
+    vanillaExtractPlugin(),
   ],
-})
+  resolve: {
+    alias: {
+      '@styles': '/src/styles',
+    },
+  },
+});
