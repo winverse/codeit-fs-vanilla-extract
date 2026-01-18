98d0ac95d2609f71608ca6b86cb160121630e26b
winverse <public.winverse@gmail.com>
2026-01-18 15:38:49 +0900

    feat: 02-theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <>App</>;
-}
+import { useState } from 'react';
+import { darkTheme, lightTheme } from '@styles/theme.css.js';
 
-export default App;
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const activeTheme = isDarkMode ? darkTheme : lightTheme;
+
+  return (
+    <div className={activeTheme}>
+      <h1>안녕하세요!</h1>
+      <button onClick={() => setIsDarkMode(!isDarkMode)}>
+        {isDarkMode ? '라이트 모드로' : '다크 모드로'}
+      </button>
+    </div>
+  );
+}
